c1ac4d17b6949449c75c69f0af4f7b9e69956f52
Damian Gunadasa <damian718@gmail.com>
2026-01-13 23:13:53 -0500

    Comments

--- src/pages/Canvas.tsx
+++ src/pages/Canvas.tsx
@@ -8,17 +8,18 @@ export default function Canvas() {
   const canvasRef = useRef<HTMLCanvasElement>(null); // create the reference to the canvas element, null since it doesnt exist untill rendered
   const [isDrawing, setIsDrawing] = useState(false); // if drawing then track mouse position, default false
   const [lastPos, setLastPos] = useState<{ x: number; y: number } | null>(null); // track mouse position when drawing, default null. lastpos since we draw from last pos to new pos of mouse
-  const socketRef = useRef<Socket | null>(null);
+  const socketRef = useRef<Socket | null>(null); // useref is a reference to elements to be used elsewhere in the code
 
-   // Connect to server
-  useEffect(() => {
-    const socket = io("http://localhost:4000");
-    socketRef.current = socket;
-
-    socket.emit("joinRoom", roomName!);
-
-    socket.on("drawing", (line) => {
-      const ctx = canvasRef.current?.getContext("2d");
+   // Connect to server. useeffect runs ONCE on mount or room change, but the socket stays active whole time, the useeffect sets up the socket
+  useEffect(() => { // this is a side effect triggered when component mounts or roomname changes
+    const socket = io("http://localhost:4000"); // sends to node.js backend server
+    socketRef.current = socket; // store current socket instance
+    // emit sends activity, on recieves
+    socket.emit("joinRoom", roomName!); // tells backend server to put me in this room so i only listen to events in this room
+    
+    // **this line is why we split up the draw into drawline and handlemousemove. If we listen with socket.on, the previous draw would need to have socket.emit inside it which results in a cycle. on trtggers emit, emit triggers on...
+    socket.on("drawing", (line) => { //listenes to drawing events and draws those events on recieveing clients canvas
+      const ctx = canvasRef.current?.getContext("2d"); // gets the drawable board to draw the line emitted, only users in the room as per backend socket.io. No need to track room here since drawing events only emitted by the backend to users in the same room.
       if (!ctx) return;
       drawLine(ctx, line.from, line.to);
     });
@@ -26,7 +27,8 @@ export default function Canvas() {
     return () => {
       socket.disconnect();
     };
-  }, [roomName]);
+  }, [roomName]); // this is how it knows to run the effect not only on mount but also when roomname changes. So roomname comes from the params which is dynamic url. so user joins a different room, the param is different etc..
+      // lets say you have user1 in canvas/test1. they use a sidebar and go canvas/test2 without reloading canvas. Thats why we need this. it will create new socket connection and disconnect previous.
 
   // Helper to get mouse position relative to canvas
   const getMousePos = (e: React.MouseEvent<HTMLCanvasElement, MouseEvent>) => {
@@ -58,6 +60,7 @@ export default function Canvas() {
 //     setLastPos(getMousePos(e));
 //   };
 
+  // this is for when the client themselves draws, now drawLine is seperated so the socket.on can draw what it recieves, and client can draw independantly.
   const handleMouseMove = (e: React.MouseEvent<HTMLCanvasElement, MouseEvent>) => {
     if (!isDrawing || !lastPos || !socketRef.current) return;
     const currentPos = getMousePos(e)!;
